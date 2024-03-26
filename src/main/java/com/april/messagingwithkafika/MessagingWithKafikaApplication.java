b191bcbc7fc00d51470abbd7a981cf6c7dd18e82
Paulo Brandão <paulo.199706@live.com>
2024-03-26 15:44:48 -0300

    create project and create docker-compose.yml

---
+++ src/main/java/com/april/messagingwithkafika/MessagingWithKafikaApplication.java
@@ -0,0 +1,13 @@
+package com.april.messagingwithkafika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWithKafikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingWithKafikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/april/messagingwithkafika/MessagingWithKafikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.april.messagingwithkafika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWithKafikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
